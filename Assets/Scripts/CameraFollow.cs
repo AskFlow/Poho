c649d539675dc7b824a081e44762190624e8f684
AskFlow <75367464+AskFlow@users.noreply.github.com>
2022-12-02 22:13:52 +0100

    Camera Bounds
    limite camera

--- Assets/Scripts/CameraFollow.cs
+++ Assets/Scripts/CameraFollow.cs
@@ -6,6 +6,7 @@ public class CameraFollow : MonoBehaviour
     public Vector3 offset;
     [Range(2,10)]
     public float smoothFactor;
+    public Vector3 minValues, maxValues;
 
     private void FixedUpdate()
     {
@@ -14,8 +15,19 @@ public class CameraFollow : MonoBehaviour
 
     void Follow()
     {
+        //Définir les min values et max values des bounds 
+
+
         Vector3 targetPosition = target.position + offset;
-        Vector3 smoothPosition = Vector3.Lerp(transform.position, targetPosition, smoothFactor*Time.fixedDeltaTime);
+        //Verifier si la targetPosition est out of bound
+        //Limiter au min et max values
+        Vector3 boundPosition = new Vector3(
+            Mathf.Clamp(targetPosition.x,minValues.x, maxValues.x), 
+            Mathf.Clamp(targetPosition.y, minValues.y, maxValues.y), 
+            Mathf.Clamp(targetPosition.z, minValues.z, maxValues.z)
+            );
+
+        Vector3 smoothPosition = Vector3.Lerp(transform.position, boundPosition, smoothFactor*Time.fixedDeltaTime);
         transform.position = smoothPosition;
     }
 }
