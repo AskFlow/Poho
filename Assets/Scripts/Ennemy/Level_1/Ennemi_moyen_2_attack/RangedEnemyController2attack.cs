a3f5385a9d57e8350468cece17d7c2676455310d
MAGNINAntoine <amagnin@gaming.tech>
2023-04-13 01:46:48 +0200

    Ennemi sur la map

--- Assets/Scripts/Ennemy/Level_1/Ennemi_moyen_2_attack/RangedEnemyController2attack.cs
+++ Assets/Scripts/Ennemy/Level_1/Ennemi_moyen_2_attack/RangedEnemyController2attack.cs
@@ -4,34 +4,39 @@ using UnityEngine;
 
 public class RangedEnemyController2attack : MonoBehaviour
 {
-    public GameObject bullet;
     public LayerMask PlayerLayer;
-    public Transform bulletpos;
-    public float timer;
-    //////////////////
+    Animator animator;
     public float moveSpeed;
     public Transform player;
     public Transform shotPoint;
     public Transform gun;
     public Vector3 startpoint;
 
+    public GameObject enemyProjectile;
 
     public float followPlayerRange;
     private bool inRange;
     public float attackRange;
+    public float attackMelee;
 
     public float startTimeBtwnShots;
+    public float startTimeBtwnattack;
+
     private float timeBtwnShots;
+    private float timeBtwnMelee;
 
-    public float attackRate = 1f;
-    float nextAttackTime = 3f;
+    private bool isAttacking = false;
+    public PlayerHealth playerHealth;
 
-   
+    private void Awake()
+    {
+        animator = GetComponent<Animator>();
+        player = GameObject.FindWithTag("Player").transform;
+    }
 
     private void Start()
     {
         startpoint = transform.position;
-       
     }
     // Update is called once per frame
     void Update()
@@ -40,51 +45,93 @@ public class RangedEnemyController2attack : MonoBehaviour
         float rotZ = Mathf.Atan2(differance.y, differance.x) * Mathf.Rad2Deg;
         gun.transform.rotation = Quaternion.Euler(0f, 0f, rotZ);
 
-        if (Vector3.Distance(transform.position, player.position) <= followPlayerRange && Vector3.Distance(transform.position, player.position) > attackRange)
-        {
-            inRange = true;
-            timer += Time.deltaTime;
 
-            if (timer > 2)
-            {
-                timer = 0;
-                shoot();
-  
-            }
-        }
-        else if (Vector3.Distance(transform.position, player.position) <= attackRange)
+        if (Vector3.Distance(transform.position, player.position) <= followPlayerRange && Vector3.Distance(transform.position, player.position) >= attackRange && !isAttacking)
         {
             inRange = true;
-            timer += Time.deltaTime;
-            if (timer > 2)
-            {
-                timer = 0;
-                AttackMelee();
-               
-            }
+            animator.SetFloat("walk", moveSpeed);
 
             Vector3 playerPos = player.position;
             playerPos.y = transform.position.y;
             Quaternion targetRotation = Quaternion.LookRotation(playerPos - transform.position);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 500f * Time.deltaTime);
         }
-        else 
+        else if ((!(transform.position.x >= startpoint.x - 1 && transform.position.x <= startpoint.x + 1) || !(transform.position.z >= startpoint.z - 0.2 && transform.position.z <= startpoint.z + 0.2)) && !isAttacking)
         {
+            Debug.Log("NOT");
             transform.position = Vector3.MoveTowards(transform.position, startpoint, moveSpeed * Time.deltaTime);
             inRange = false;
+            animator.SetFloat("walk", moveSpeed);
 
+            Vector3 spawnPos = startpoint;
+            spawnPos.y = transform.position.y;
+            Quaternion targetRotation = Quaternion.LookRotation(spawnPos - transform.position);
+            transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 500f * Time.deltaTime);
+        }
+        else
+        {
+            animator.SetFloat("walk", 0f);
+        }
+
+        if (((transform.position.x >= startpoint.x - 1 && transform.position.x <= startpoint.x + 1) && (transform.position.z >= startpoint.z - 0.2 && transform.position.z <= startpoint.z + 0.2)) || isAttacking)
+        {
             Vector3 playerPos = player.position;
             playerPos.y = transform.position.y;
             Quaternion targetRotation = Quaternion.LookRotation(playerPos - transform.position);
             transform.rotation = Quaternion.RotateTowards(transform.rotation, targetRotation, 500f * Time.deltaTime);
-        }      
+        }
+
+        if (Vector3.Distance(transform.position, player.position) <= attackRange && Vector3.Distance(transform.position, player.position) >= attackMelee)
+        {
+            if (timeBtwnShots <= 0 && !isAttacking)
+            {
+                isAttacking = true;
+                animator.SetTrigger("attack");
+
+                timeBtwnShots = startTimeBtwnShots;
+            }
+            else
+            {
+                timeBtwnShots -= Time.deltaTime;
+            }
+
+        }
+
+        IEnumerator attack()
+        {
+            yield return new WaitForSeconds(2);
+            playerHealth.ApplyDamage(20);
+        }
+
+        if (Vector3.Distance(transform.position, player.position) <= attackMelee)
+        {
+            if (timeBtwnMelee <= 0 && !isAttacking)
+            {
+                animator.SetTrigger("attackrange");
+                
+                timeBtwnMelee = startTimeBtwnattack;
+                Collider[] hitPlayer = Physics.OverlapSphere(transform.position, attackRange, PlayerLayer);
+                for (int i = 0; i < hitPlayer.Length; i++)
+                {
+                    if (hitPlayer[i].tag == "Player")
+                        StartCoroutine(attack());
+                }
+            }
+            else
+            {
+                timeBtwnMelee -= Time.deltaTime;
+            }
+
+        }
     }
 
     void FixedUpdate()
     {
         if (inRange)
         {
-            transform.position = Vector3.MoveTowards(transform.position, player.position, moveSpeed * Time.deltaTime);
+            Vector3 playerPos = player.position;
+            //playerPos.y = transform.position.y;
+            transform.position = Vector3.MoveTowards(transform.position, playerPos, moveSpeed * Time.deltaTime);
         }
     }
 
@@ -93,32 +140,14 @@ public class RangedEnemyController2attack : MonoBehaviour
         Gizmos.DrawWireSphere(transform.position, followPlayerRange);
         Gizmos.color = Color.red;
         Gizmos.DrawWireSphere(transform.position, attackRange);
-    }
-     public void shoot()
-    {      
-        Instantiate(bullet, bulletpos.position, Quaternion.identity);
-        
-        Collider[] hitPlayer = Physics.OverlapSphere(transform.position, followPlayerRange, PlayerLayer);
-
-        // Appliquer les damages
-        foreach (Collider Player in hitPlayer)
-        {           
-           //applydamage
-        }
+        Gizmos.color = Color.blue;
+        Gizmos.DrawWireSphere(transform.position, attackMelee);
     }
 
-
-    void AttackMelee()
+    void Attack()
     {
-        // Jouer l'animation de l'attaque (à l'avenir)
-        
-        // Detecter les ennemies dans la range
-        Collider[] hitPlayer = Physics.OverlapSphere(transform.position, attackRange, PlayerLayer);
-       
-        // Appliquer les damages
-        foreach (Collider Player in hitPlayer)
-        {            
-            Player.GetComponent<PlayerHealth>().ApplyDamage(100);
-        }
+        Instantiate(enemyProjectile, shotPoint.position, shotPoint.transform.rotation);
+
+        isAttacking = false;
     }
 }
